316ed5d75d5acf94f7a15ed91a1f5b3d6684541b
bgoonz <bryan.guner@gmail.com>
2021-09-08 05:00:42 +0000

    hi

---
+++ _html/version1/resources/webpack---/common/lib/utils/get-sandbox-name.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.getSandboxName = void 0;
+exports.getSandboxName = ({
+    alias,
+    id,
+    title,
+}) => title || alias || id;
---
+++ _html/version1/resources/webpack---/common/lib/utils/range.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.range = void 0;
+exports.range = (size, startAt = 0, skip = 1) => [...Array(size).keys()].map(i => i * skip + startAt);
